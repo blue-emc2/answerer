78a3082c67cc162981913b6dd4e729b4ed46f26d
Shunsuke Yamazaki <s.yamazaki@everyleaf.com>
2020-01-09 23:00:34 +0900

    回答画面にパーツを置く
    見た目の調整は後日

--- src/components/AnswerScreen.tsx
+++ src/components/AnswerScreen.tsx
@@ -1,22 +1,52 @@
-import React from 'react';
-import { View, Text, Button } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
 import {
   NavigationStackProp,
   NavigationStackScreenComponent,
 } from 'react-navigation-stack';
+import { TextInput } from 'react-native-gesture-handler';
 
 type Props = {
   navigation: NavigationStackProp;
   name: string;
 };
 
-const AnswerScreen: NavigationStackScreenComponent<Props> = ({
-  navigation,
-}) => {
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    flex: 1,
+    justifyContent: 'center',
+  },
+  title: {
+    textAlign: 'center',
+    marginVertical: 8,
+  },
+  input: {
+    height: 128,
+    borderColor: 'gray',
+    borderWidth: 1,
+    width: 256,
+    marginVertical: 8,
+  },
+});
+
+const AnswerScreen: NavigationStackScreenComponent<Props> = () => {
+  const [value, onChangeText] = useState('');
+  const [editable, setEditable] = useState(true);
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Answer Screen</Text>
-      <Button title="送信" onPress={() => navigation.navigate('Answer')} />
+    <View style={styles.container}>
+      <Text>ここに問題文を表示する</Text>
+      <TextInput
+        style={styles.input}
+        value={value}
+        onChangeText={text => onChangeText(text)}
+        multiline
+        editable={editable}
+      />
+      <Button title="送信" onPress={() => setEditable(false)} />
+      <Button title="やり直す" onPress={() => setEditable(true)} />
     </View>
   );
 };
